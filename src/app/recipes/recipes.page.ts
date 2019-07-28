55d578aaa7754c113e5b4f67a1e70dcf4ed2b1ca
uniqueshiv <uniqueshivchauhan@gmail.com>
2019-07-28 20:16:01 +0530

    sss

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RecipesService } from './recipes.service';
 import Recipe from './recipe.model';
 
@@ -8,15 +8,33 @@ import Recipe from './recipe.model';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit,OnDestroy {
   recipes: Recipe[];
 
   constructor(private recipeService:RecipesService) { }
 
   ngOnInit() {
-    this.recipes = this.recipeService.getAllRecipes();
     console.log("recipes page recipes lists:");
-    console.log(this.recipes)
+    //console.log(this.recipes)
+  }
+  ionViewWillEnter(){
+    this.recipes = this.recipeService.getAllRecipes();
+    console.log("ioniviewWill enter");
+  }
+  
+  ionViewDidEnter(){
+    console.log("ionviewdidenter");
+  }
+  
+  ionViewWillLeave(){
+    console.log("ionviewwill leave");
+  }
+  ionViewDidLeave(){
+    console.log("ionviewdid leave");
+  }
+
+  ngOnDestroy(){
+    console.log('ngdestroy');
   }
 
 }
